d6fbed1fb270d824ed859baf5ee081544d21e379
Luka Ivanović <luka@ljudi.org>
2025-04-02 19:17:28 +0200

    feat: Implement cli feature
    Also, bump go version

--- cmd/diceware/main.go
+++ cmd/diceware/main.go
@@ -1,22 +1,20 @@
-// Copyright © 2023- Luka Ivanović
+// Copyright © 2023-2025 Luka Ivanović
 // This code is licensed under the terms of the MIT licence (see LICENCE for details)
 
 package main
 
 import (
 	"fmt"
-	"math"
-	"math/rand"
 	"os"
 	"strconv"
-	"strings"
 )
 
 func main() {
 	args := os.Args[1:]
 	if len(args) > 1 {
 		printUsage()
-		os.Exit(1)
+		os.Exit(3)
+		// "Invalid argument: An incorrect or missing argument was provided to the script."
 	}
 	length := 8
 	if len(args) == 1 {
@@ -24,32 +22,13 @@ func main() {
 		length, err = strconv.Atoi(args[0])
 		if err != nil {
 			printUsage()
-			os.Exit(1)
+			os.Exit(3)
+			// "Invalid argument: An incorrect or missing argument was provided to the script."
 		}
 	}
-	if length < 1 {
-		printUsage()
-		os.Exit(1)
-	}
-	passphrase := make([]string, length)
-	for i := 0; i < length; i += 1 {
-		passphrase[i] = words[rollDices()]
-	}
-	fmt.Println(strings.Join(passphrase, " "))
+	fmt.Println(NewPassphraseWithLength(length))
 }
 
 func printUsage() {
-	fmt.Fprintln(os.Stderr, "Usage: diceware <length>\nDefault length is 8 words, argument <length> is optional and must be positive number.")
-}
-
-func rollDice() int {
-	return rand.Intn(6) + 1
-}
-
-func rollDices() int {
-	result := 0
-	for i := 0; i < 5; i += 1 {
-		result += rollDice() * (int(math.Pow10(5 - i - 1)))
-	}
-	return result
+	fmt.Fprintln(os.Stderr, "Usage: diceware <length>\n\nDefault length is 8 words, argument <length> is optional and must be in range [1, 256].")
 }
